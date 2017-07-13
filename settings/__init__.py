021b19eff39ee69d25f3ecfe898e69ced4e3ffb6
aslan <bloogrox@gmail.com>
2017-07-13 22:00:58 +0300

    ci settings

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,6 +1,13 @@
-try:
-    from .local import *  # NOQA
-    print("Using LOCAL settings")
-except ImportError:
-    from .prod import *  # NOQA
-    print("Using PROD settings")
+import os
+
+
+if os.environ["ENV_TYPE"] == "ci":
+    from .ci import *  # NOQA
+    print("Using CI settings")
+else:
+    try:
+        from .local import *  # NOQA
+        print("Using LOCAL settings")
+    except ImportError:
+        from .prod import *  # NOQA
+        print("Using PROD settings")
