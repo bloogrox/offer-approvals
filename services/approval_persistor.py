ef0037b0b3ca2f445b661c419062606d2e710cfe
aslan <bloogrox@gmail.com>
2017-07-14 00:40:34 +0300

    persist record in db. closes #9

--- services/approval_persistor.py
+++ services/approval_persistor.py
@@ -1,10 +1,16 @@
 from nameko.rpc import rpc
 
+from app import mongo_database
+
 
 class ApprovalPersistorService:
     name = "approval_persistor_service"
 
     @rpc
-    def persist(self):
-        # create or replace object with a new one
-        pass
+    def persist(self, approval):
+        try:
+            (mongo_database.approvals
+             .replace_one({"_id": approval["id"]}, approval, upsert=True))
+        except Exception as e:
+            print("ApprovalPersistorService.persist: "
+                  f"exception - {e}")
